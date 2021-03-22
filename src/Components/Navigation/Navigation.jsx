ecf19f38ded1a43643978edd11be7511a1a12ec6
Nicolas Salas <nicolassalas@Nicolass-MBP.lan>
2021-03-22 14:29:09 +0100

    changed the properties of how the drop down menu button changed

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -9,21 +9,34 @@ const Navigation = () => {
       toggle between hiding and showing the dropdown content */
   function dropDownMenu() {
     document.getElementById('myDropdown').classList.toggle('show');
+    toggleMenuButton();
   }
 
   // Close the dropdown menu if the user clicks outside of it
   window.onclick = function (event) {
     if (!event.target.matches('.drop-button-part')) {
       const dropdowns = document.getElementsByClassName('dropdown-content');
-      var i;
+      let i;
       for (i = 0; i < dropdowns.length; i++) {
         const openDropdown = dropdowns[i];
         if (openDropdown.classList.contains('show')) {
           openDropdown.classList.remove('show');
+          toggleMenuButton();
         }
       }
     }
   };
+
+  function toggleMenuButton() {
+    const element = document.getElementById('myDropdown');
+    if (element.classList.contains('show')) {
+      console.log(document.getElementsByClassName('button-container'));
+      document.getElementsByClassName('button-container')[0].classList.add('button-in-focus');
+    } else {
+      console.log(document.getElementsByClassName('button-container'));
+      document.getElementsByClassName('button-container')[0].classList.remove('button-in-focus');
+    }
+  }
 
   return (
     <div>
@@ -33,7 +46,7 @@ const Navigation = () => {
         </NavLink>
         <div className='nav-mobile'>
           <button type='button' onClick={dropDownMenu} className='drop-button drop-button-part'>
-            <div className='button-container'>
+            <div className='button-container '>
               <div className='button-line line1'></div>
               <div className='button-line line2'></div>
               <div className='button-line line3'></div>
